065d6af1d911f6d0687c7d6dadc8c582940f780c
rinsewester <renzzz@gmail.com>
2016-06-19 23:24:35 +0200

    Intial verion of HSDF edge in CLaSH, output shows an issue still...

--- codegen/clash_code/hsdfedge.hs
+++ codegen/clash_code/hsdfedge.hs
@@ -4,18 +4,29 @@ module HSDFEdge where
 
 -- imports
 import CLaSH.Prelude
+import Debug.Trace
 
 --type definitions
 type Byte = Unsigned 8
+type Elm = Byte
+type ElmVec = Vec 8 Elm
+type Ptr = Unsigned 4
+
+-- ignore the most significant bit by shifting up and down
+ptr2ind ptr = shiftR (shiftL ptr 1) 1
 
 
-hsdfedge :: Byte -> Byte -> (Byte, Byte)
-hsdfedge s i = (s', o)
+hsdfedge :: (ElmVec, Ptr, Ptr) -> (Elm, Bool, Bool) -> ((ElmVec, Ptr, Ptr), (Elm, Bool, Bool, ElmVec))
+hsdfedge (elms, rptr, wptr) (datain, prod, cons) = ((elms', rptr', wptr'), (dataout, empty, full, elms'))
     where
-        o = s
-        s' = i
+        elms' = if cons then replace (ptr2ind wptr) datain elms else elms
+        rptr' = if cons then rptr + 1 else rptr
+        wptr' = if prod then wptr + 1 else wptr
+        dataout = elms !! (ptr2ind rptr)
+        empty = rptr == wptr
+        full = (msb rptr /= msb wptr) && (ptr2ind rptr == ptr2ind wptr)
 
-hsdfedgeL = mealy hsdfedge 0
 
-hsdfedgeLsim = simulate hsdfedgeL [1,2,3,4,5]
+hsdfedgeL = mealy hsdfedge (repeat 0, 0, 0)
 
+write_sim_res = simulate hsdfedgeL [(99, False, True), (99, False, False), (99, False, False)]
