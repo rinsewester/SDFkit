e1926a504c39efb271e6eedf92b5b728cf5f49ad
rinsewester <renzzz@gmail.com>
2016-06-21 18:06:25 +0200

    Initial commit of graph with hsdfedges and nodes.

---
+++ codegen/clash_code/hsdfgraph.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE RecordWildCards, DataKinds #-}
+
+module HSDFGraph where
+
+-- imports
+import CLaSH.Prelude
+import Debug.Trace
+import qualified Data.List as L
+
+--type definitions
+type Byte = Unsigned 8
+
+-- TODO make cicle graph here using the hsdfedges and hsdfnodes
+
+
