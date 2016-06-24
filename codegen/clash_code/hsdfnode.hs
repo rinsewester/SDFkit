51b3fb013148c21de589a4580500170244022cdc
rinsewester <renzzz@gmail.com>
2016-06-24 12:26:58 +0200

    Defined all the nodes for a more complicated graph.

--- codegen/clash_code/hsdfnode.hs
+++ codegen/clash_code/hsdfnode.hs
@@ -16,7 +16,7 @@ hsdfnode_1_1 f (firecounter, phase) (datain, empty, full) = ((firecounter', phas
         fire = not empty && not full
         firecounter' = if fire then firecounter + 1 else firecounter
         phase_max = 0
-        phase' = if fire then (if phase<phase_max then phase + 1 else 0) else phase_max
+        phase' = if fire then (if phase < phase_max then phase + 1 else 0) else phase_max
         dataout = f datain
 
 func = id :: Byte -> Byte
@@ -33,13 +33,48 @@ hsdfnode_0_1 f (firecounter, phase) (full) = ((firecounter', phase'), (dataout, 
         fire = not full
         firecounter' = if fire then firecounter + 1 else firecounter
         phase_max = 0
-        phase' = if fire then (if phase<phase_max then phase + 1 else 0) else phase_max
+        phase' = if fire then (if phase < phase_max then phase + 1 else 0) else phase_max
         dataout = f firecounter phase
 
 func2 :: Cntr -> Cntr -> Cntr
 func2 firecounter phase = firecounter
 
 hsdfnode_0_1L = mealy (hsdfnode_0_1 func2) (0, 0)
+
+
+
+
+
+hsdfnode_1_2 :: (a -> Cntr -> Cntr ->(b, c)) -> (Cntr, Cntr) -> (a, Bool, Bool, Bool) -> ((Cntr, Cntr), (b, c, Bool))
+hsdfnode_1_2 f (firecounter, phase) (datain0, empty0, full0, full1) = ((firecounter', phase'), (dataout0, dataout1, fire))
+    where
+        fire = not empty0 && not full0 && not full1
+        firecounter' = if fire then firecounter + 1 else firecounter
+        phase_max = 0
+        phase' = if fire then (if phase < phase_max then phase + 1 else 0) else phase_max
+        (dataout0, dataout1) = f datain0 firecounter phase
+
+func3 :: Cntr -> Cntr -> Cntr -> (Cntr, Cntr)
+func3 datain0 firecounter phase = (datain0, datain0)
+
+hsdfnode_1_2L = mealy (hsdfnode_1_2 func3) (0, 0)
+
+
+
+
+hsdfnode_2_0 :: (a -> b -> Cntr -> Cntr -> ()) -> (Cntr, Cntr) -> (a, b, Bool, Bool) -> ((Cntr, Cntr), (Bool))
+hsdfnode_2_0 f (firecounter, phase) (datain0, datain1, empty0, empty1) = ((firecounter', phase'), (fire))
+    where
+        fire = not empty0 && not empty1
+        firecounter' = if fire then firecounter + 1 else firecounter
+        phase_max = 0
+        phase' = if fire then (if phase < phase_max then phase + 1 else 0) else phase_max
+        res = f datain0 datain1 firecounter phase
+
+func4 :: Cntr -> Cntr -> Cntr -> Cntr -> ()
+func4 datain0 datain1 firecounter phase = ()
+
+hsdfnode_2_0L = mealy (hsdfnode_2_0 func4) (0, 0)
 
 
 
