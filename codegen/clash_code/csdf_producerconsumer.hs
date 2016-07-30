8978fe5df05fc6de802ac187c4a2c6c59d0f19da
rinsewester <renzzz@gmail.com>
2016-07-30 10:05:34 +0200

    CSDF graph example compiles. Needs verification though.

--- codegen/clash_code/csdf_producerconsumer.hs
+++ codegen/clash_code/csdf_producerconsumer.hs
@@ -18,7 +18,7 @@ f_Pr firecounter phase = res
   where
     res = repeat (resize firecounter)
 
-f_Cr :: Vec 3 Byte -> Cntr -> Cntr -> ()
+f_Cr :: Vec 2 Byte -> Cntr -> Cntr -> ()
 f_Cr x firecounter phase = res
   where
     res = ()
@@ -26,9 +26,9 @@ f_Cr x firecounter phase = res
 
 -- Definitions for the nodes:
 n_Pr :: (Cntr -> Cntr -> (a)) -> (Cntr, Cntr) -> (Bool) -> ((Cntr, Cntr), (a, Bool))
-n_Pr f (firecounter, phase) (full0) = ((firecounter', phase'), (dataout0, fire))
+n_Pr f (firecounter, phase) (canwrt0) = ((firecounter', phase'), (dataout0, fire))
     where
-        fire = not full0
+        fire = canwrt0
         firecounter' = if fire then firecounter + 1 else firecounter
         phase_max = 0
         phase' = if fire then (if phase < phase_max then phase + 1 else 0) else phase_max
@@ -37,9 +37,9 @@ n_Pr f (firecounter, phase) (full0) = ((firecounter', phase'), (dataout0, fire))
 n_PrL = mealy (n_Pr f_Pr) (0, 0)
 
 n_Cr :: (a -> Cntr -> Cntr -> ()) -> (Cntr, Cntr) -> (a, Bool) -> ((Cntr, Cntr), (Bool))
-n_Cr f (firecounter, phase) (datain0, empty0) = ((firecounter', phase'), (fire))
+n_Cr f (firecounter, phase) (datain0, canrd0) = ((firecounter', phase'), (fire))
     where
-        fire = not empty0
+        fire = not canrd0
         firecounter' = if fire then firecounter + 1 else firecounter
         phase_max = 0
         phase' = if fire then (if phase < phase_max then phase + 1 else 0) else phase_max
@@ -50,23 +50,23 @@ n_CrL = mealy (n_Cr f_Cr) (0, 0)
 
 
 -- The actual graph
-graph :: Signal Bool -> Signal (Bool, Cntr, Bool, Cntr, Bool, Cntr)
+graph :: Signal Bool -> Signal (Bool, Bool)
 graph inputs = outputs
     where
         -- Instantiations of nodes
-        (n_Pr_dataout0, n_Pr_fire) = unbundle $ n_PrL n_Pr_full0
-        n_Cr_fire = n_CrL $ bundle (n_Cr_datain0, n_Cr_empty0)
+        (n_Pr_dataout0, n_Pr_fire) = unbundle $ n_PrL n_Pr_canwrt0
+        n_Cr_fire = n_CrL $ bundle (n_Cr_datain0, n_Cr_canrd0)
         
         -- Define the edges
-        (e_Pr_Int_dataout, e_Pr_Int_empty, e_Pr_Int_full) = unbundle $ e_Pr_IntL $ bundle (e_Pr_Int_datain, e_Pr_Int_rd, e_Pr_Int_wrt)
-
+        (e_Pr_Cr_dataout, e_Pr_Cr_canread, e_Pr_Cr_canwrite, e_Pr_Cr_err) = unbundle $ csdfedgeL $ bundle (e_Pr_Cr_datain, e_Pr_Cr_rd, e_Pr_Cr_wrt)
         
         -- Connect the node and edge signals
-        (nodeinps) = ........
-        (edgeinps) = ......
+        n_Pr_canwrt0 = e_Pr_Cr_canwrite
+        (n_Cr_datain0, n_Cr_canrd0) = (e_Pr_Cr_dataout, e_Pr_Cr_canread)
+        (e_Pr_Cr_datain, e_Pr_Cr_rd, e_Pr_Cr_wrt) = (n_Pr_dataout0, n_Cr_fire, n_Pr_fire)
 
         -- Output for debugging
-        outputs = bundle ()
+        outputs = bundle (e_Pr_Cr_err, e_Pr_Cr_err)
 
 
 -- Simulate the circuit for 128 clock-cycles
