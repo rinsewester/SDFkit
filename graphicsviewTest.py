dedf82b06ef4a1439e17d02359bee6655852d8cb
SanderGiesselink <sandergiesselink@gmail.com>
2016-08-08 16:45:03 +0200

    Add files via upload

--- graphicsviewTest.py
+++ graphicsviewTest.py
@@ -9,22 +9,25 @@ author: Sander Giesselink
 """
 
 import sys
-from PyQt5.QtWidgets import QWidget, QDockWidget, QGraphicsView, QApplication, QSplitter, QSlider, QHBoxLayout, QVBoxLayout, QGridLayout, QFrame, QLabel, QToolButton, QButtonGroup, QGraphicsScene
-from PyQt5.QtCore import Qt, QSize, QObject, pyqtSignal, QRectF
-from PyQt5.QtGui import QIcon, QTransform
+from PyQt5.QtWidgets import QWidget, QDockWidget, QGraphicsView, QGraphicsScene, QApplication, QSplitter, QSlider, QHBoxLayout, QVBoxLayout, QGridLayout, QFrame, QLabel, QToolButton, QButtonGroup, QGraphicsScene, QGraphicsItem
+from PyQt5.QtCore import Qt, QSize, QObject, pyqtSignal, QRectF, QPointF
+from PyQt5.QtGui import QIcon, QTransform, QColor, QPainter, QBrush, QFont
+from node import*
 
 class GraphicsView(QGraphicsView):
 
-        def WheelEvent(self, event):
-                if event.modifiers() & Qt.ControlModifier:
-                	if event.delta() > 0:
-                		view.zoomIn(6)
-                	else:
-                		view.zoomOut(6)
+    def wheelEvent(self, event):
+        if event.modifiers() & Qt.ControlModifier:
+            if event.angleDelta().y() > 0:
+                GraphWidget.zoomIn(6)
+                print('zoomIN')
+            else:
+                GraphWidget.zoomOut(6)
+        
+            event.accept()
+        else:
+            super().wheelEvent(event)
 
-                	event.accept()
-                else:
-                	QGraphicsView(self, event)
 
 
 class GraphWidget(QWidget):
@@ -38,24 +41,42 @@ class GraphWidget(QWidget):
     def initUI(self):
 
         #setFrameStyle(Sunken | StyledPanel)
-        self.graphicsView = GraphicsView()
+        self.graphicsView = QGraphicsView()
         self.graphicsView.setDragMode(QGraphicsView.RubberBandDrag)
         self.graphicsView.setOptimizationFlags(QGraphicsView.DontSavePainterState)
         self.graphicsView.setViewportUpdateMode(QGraphicsView.SmartViewportUpdate)
         self.graphicsView.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.graphicsView.setRenderHint(QPainter.Antialiasing, True)
 
+        #Make a graphics scene
+        scene = QGraphicsScene()
+        self.graphicsView.setScene(scene)
+
+
+        testNode = Node(QColor(255,0,0),50 , 50)
+        node = scene.addItem(testNode)
+
+
+
+        text = scene.addText('Test', QFont('Arial', 20))
+        rect = scene.addRect(100, 100, 80, 60)
+        text.setFlag(QGraphicsItem.ItemIsMovable)
+        rect.setFlag(QGraphicsItem.ItemIsMovable)
+
+
+        #UI for the graphicsView
         size = 16
         iconSize = QSize(size, size)
 
         self.zoomInButton = QToolButton(self)
         self.zoomInButton.setAutoRepeat(True)
-        #self.zoomInButton.setAutorepeatInterval(33)
+        self.zoomInButton.setAutoRepeatInterval(33)
         self.zoomInButton.setAutoRepeatDelay(0);
         self.zoomInButton.setIcon(QIcon('images/zoomin.png'));
         self.zoomInButton.setIconSize(iconSize);
         self.zoomOutButton = QToolButton(self)
         self.zoomOutButton.setAutoRepeat(True);
-        #self.zoomOutButton.setAutoRepeatInterval(33);
+        self.zoomOutButton.setAutoRepeatInterval(33);
         self.zoomOutButton.setAutoRepeatDelay(0);
         self.zoomOutButton.setIcon(QIcon("images/zoomout.png"));
         self.zoomOutButton.setIconSize(iconSize);
@@ -75,34 +96,8 @@ class GraphWidget(QWidget):
         zoomSliderLayout.addWidget(self.zoomOutButton)
         zoomSliderLayout.addWidget(self.resetButton)       
 
-        #Label layout
-        labelLayout = QHBoxLayout()
-        # label = QLabel('Designer')
-        # label2 = QLabel('Pointer Mode')
-        # selectModeButton = QToolButton(self)
-        # selectModeButton.setText('Select')
-        # selectModeButton.setCheckable(True)
-        # selectModeButton.setChecked(True)
-        # dragModeButton = QToolButton(self)
-        # dragModeButton.setText('Drag')
-        # dragModeButton.setCheckable(True)
-        # dragModeButton.setChecked(False)
-
-        # buttonGroup = QButtonGroup()
-        # buttonGroup.setExclusive(True)
-        # buttonGroup.addButton(selectModeButton)
-        # buttonGroup.addButton(dragModeButton)
-
-        # labelLayout.addWidget(label)
-        # labelLayout.addStretch(1)
-        # labelLayout.addWidget(label2)
-        # labelLayout.addWidget(selectModeButton)
-        # labelLayout.addWidget(dragModeButton)
-        # labelLayout.addStretch(1)
-
         #Final layout
         topLayout = QGridLayout()
-        topLayout.addLayout(labelLayout, 0, 0)
         topLayout.addWidget(self.graphicsView, 1, 0)
         topLayout.addLayout(zoomSliderLayout, 1, 1)
         self.setLayout(topLayout)
@@ -112,6 +107,8 @@ class GraphWidget(QWidget):
         self.zoomSlider.valueChanged.connect(self.setupMatrix)
         self.zoomInButton.clicked.connect(self.zoomIn)
         self.zoomOutButton.clicked.connect(self.zoomOut)
+
+
 
 
     def resetView(self):
@@ -128,8 +125,9 @@ class GraphWidget(QWidget):
 
 
     def setupMatrix(self):
-        scale = (2.0 ** (self.zoomSlider.value() - 250) / 50.0)
-        scale = scale * scale
+        scale = 2.0 ** ((self.zoomSlider.value() - 250) / 50.0)
+        print((self.zoomSlider.value() - 250) / 50.0)
+        #scale = scale * scale
 
         transform = QTransform()
         transform.scale(scale, scale)
@@ -139,10 +137,20 @@ class GraphWidget(QWidget):
 
 
     def zoomIn(self, level):
-        level = 1
         self.zoomSlider.setValue(self.zoomSlider.value() + level)
 
 
     def zoomOut(self, level):
-        level = 1
         self.zoomSlider.setValue(self.zoomSlider.value() - level)
+
+    def wheelEvent(self, event):
+        if event.modifiers() & Qt.ControlModifier:
+            if event.angleDelta().y() > 0:
+                self.zoomIn(6)
+                print('zoomIN')
+            else:
+                self.zoomOut(6)
+        
+            event.accept()
+        else:
+            super().wheelEvent(event)
--- node.py
+++ node.py
@@ -9,8 +9,59 @@ author: Sander Giesselink
 """
 
 import sys
+from PyQt5.QtWidgets import QWidget, QGraphicsItem
+from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QColor, QPainter, QBrush, QPainterPath
 
-class Node(QWidget):
+class Node(QGraphicsItem):
 
-	def __init__(self):
+    def __init__(self, color, x, y):
         super().__init__()
+        self.color = color
+        self.x = x
+        self.y = y
+
+        #self.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
+        print(self.flags())
+        self.setFlag(QGraphicsItem.ItemIsMovable)
+        #self.setAcceptHoverEvents(True)
+        print('node created')
+
+
+    def boundingRect(self):
+        return QRectF(0, 0, 100, 60)
+
+    
+    def shape(self):
+    	path = QPainterPath()
+    	path.addRect(0, 0, 100, 60)
+    	return path
+
+    
+    def paint(self, painter, option, widget):
+        #painter = QPainter(self)
+        
+        color = QColor(0, 0, 0)
+        painter.setPen(color)
+      
+        brush = QBrush(QColor(255, 0, 0))
+        painter.setBrush(brush)
+        painter.drawRoundedRect(0, 0, 100, 60, 10, 10)
+
+
+    def mousePressEvent(self, event):
+    	super().mousePressEvent(event)
+    	self.update()
+
+
+    def mouseMoveEvent(self, event):
+    	#Code for moving nodes goes here
+    	self.update()
+    	super().mouseMoveEvent(event)
+
+
+    def mouseReleaseEvent(self, event):
+        super().mouseReleaseEvent(event)
+        self.update()
+
+
