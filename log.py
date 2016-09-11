81d30486db9b8d7bfb08707da3507ed4688bcdc5
rinsewester <renzzz@gmail.com>
2016-09-12 00:04:21 +0200

    Added a class for storing logmessages.

---
+++ log.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Class for logging SDFkit messages
+
+author: Rinse Wester
+
+"""
+
+import datetime as dt
+
+class Log(object):
+	"""Log class to store logmessages from SDFkit"""
+
+	ERROR = 4
+	CRITICAL = 3
+	WARNING = 2
+	INFO = 1
+
+	_msg_list = []
+
+	def addLogMessage(msgtype, msgtext):
+		"""Add a message to the list of log messages
+
+		msgtype is the type of message and must be one of: Log.ERROR, Log.CRITICAL,
+		Log.WARNING, Log.INFO.
+
+		msgtext contains the actual info message and should be human readable."""
+		Log._msg_list.append((dt.datetime.now(), msgtype, msgtext))
+
+	def getLogMessages(mintype=INFO):
+		"""Get all recorded logmessages
+
+		When mintype is given, only message with a minimum severity are returned.
+		For example when mintype = CRITICAL, only CRITICAL and ERROR messages are given.
+		Similarly, when mintype = INFO, all messages are returned"""
+		return list(filter(lambda tpl: tpl[1] >= mintype, Log._msg_list))
+
+	def clearMessages():
+		"""Remove all logmessages."""
+		Log._msg_list.clear()
+
