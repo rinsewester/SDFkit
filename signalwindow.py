02d3b79b12ae92be42cdf0a132d2e52ab2013468
rinsewester <renzzz@gmail.com>
2016-09-18 14:08:38 +0200

    Highlighting of cycles completed.

--- signalwindow.py
+++ signalwindow.py
@@ -125,7 +125,7 @@ class SignalLogWidget(QWidget):
         self.update()
 
     def mouseMoveEvent(self, event):
-        
+
         xpos = event.pos().x()
         self.hoveringCycle = floor((xpos + self.TRANSITION_WIDTH / 2) / (self.STATE_WIDTH * self.zoomFactor + self.TRANSITION_WIDTH))
         self.update()
@@ -149,9 +149,12 @@ class SignalLogWidget(QWidget):
 
         for i, tokenData in enumerate(self.tokenData[:-1]):
 
+            curhovering = i == self.hoveringCycle
+            nxthovering = i + 1 == self.hoveringCycle
+
             beginCoor = i * (curStatewidth + self.TRANSITION_WIDTH)
 
-            self._drawState(qp, beginCoor, curStatewidth, tokenData, i == self.hoveringCycle)
+            self._drawState(qp, beginCoor, curStatewidth, tokenData, curhovering)
 
             if tokenData == self.tokenData[i + 1]:
                 if tokenData == []:
@@ -167,74 +170,108 @@ class SignalLogWidget(QWidget):
                     transType = 'data2empty'
 
             self._drawTransition(qp, beginCoor + curStatewidth,
-                                 self.TRANSITION_WIDTH, transType)
+                                 self.TRANSITION_WIDTH, transType, curhovering, nxthovering)
 
         qp.end()
 
-    def _drawTransition(self, qp, xpos, width, ttype):
+    def _drawTransition(self, qp, xpos, width, ttype, hoverLeft, hoverRight):
 
         # Set pen and brush style
-        pen = QPen(SignalLogWidget.SIGNAL_COLOR)
+        color = SignalLogWidget.SIGNAL_COLOR
+        pen = QPen(color)
         pen.setWidth(2)
-        brush = QBrush(SignalLogWidget.SIGNAL_COLOR)
+        brush = QBrush(color)
         qp.setPen(pen)
         qp.setBrush(brush)
+        pen.setStyle(Qt.NoPen)
+        qp.setPen(pen)
 
         size = self.size()
         h = size.height()
 
         if ttype == 'sameData':
-            pen.setStyle(Qt.NoPen)
-            qp.setPen(pen)
-            qp.drawRect(xpos, 3, width, h - 6)
+            if hoverLeft:
+                brush.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                brush.setColor(self.SIGNAL_COLOR)
+            qp.setBrush(brush)
+            qp.drawRect(xpos, 2, width / 2, h - 4)
 
-            pen.setStyle(Qt.SolidLine)
-            qp.setPen(pen)
-            qp.drawLine(xpos, 3, xpos + width, 3)
-            qp.drawLine(xpos, h - 3, xpos + width, h - 3)
+            if hoverRight:
+                brush.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                brush.setColor(self.SIGNAL_COLOR)
+            qp.setBrush(brush)
+            qp.drawRect(xpos + width / 2, 2, width / 2, h - 4)
+
         elif ttype == 'dataChange':
-            pen.setStyle(Qt.NoPen)
-            qp.setPen(pen)
-
-            points = [QPoint(xpos, 3), QPoint(xpos, h - 3),
-                      QPoint(xpos + width, 3), QPoint(xpos + width, h - 3)]
+            if hoverLeft:
+                brush.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                brush.setColor(self.SIGNAL_COLOR)
+            qp.setBrush(brush)
+            points = [QPoint(xpos, 2), QPoint(xpos, h - 2),
+                      QPoint(xpos + width / 2, h / 2)]
             qp.drawPolygon(QPolygon(points))
 
-            pen.setStyle(Qt.SolidLine)
-            qp.setPen(pen)
-            qp.drawLine(xpos, 3, xpos + width, h - 3)
-            qp.drawLine(xpos, h - 3, xpos + width, 3)
+            if hoverRight:
+                brush.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                brush.setColor(self.SIGNAL_COLOR)
+            qp.setBrush(brush)
+            points = [QPoint(xpos + width, 2), QPoint(xpos + width, h - 2),
+                      QPoint(xpos + width / 2, h / 2)]
+            qp.drawPolygon(QPolygon(points))
+
         elif ttype == 'empty2data':
-            pen.setStyle(Qt.NoPen)
-            qp.setPen(pen)
-
+            if hoverRight:
+                brush.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                brush.setColor(self.SIGNAL_COLOR)
             points = [QPoint(xpos + width / 2, h / 2),
-                      QPoint(xpos + width, 3), QPoint(xpos + width, h - 3)]
+                      QPoint(xpos + width, 2), QPoint(xpos + width, h - 2)]
+            qp.setBrush(brush)
             qp.drawPolygon(QPolygon(points))
 
+            if hoverLeft:
+                pen.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                pen.setColor(self.SIGNAL_COLOR)
             pen.setStyle(Qt.SolidLine)
             qp.setPen(pen)
             qp.drawLine(xpos, h / 2, xpos + width / 2, h / 2)
-            qp.drawLine(xpos + width / 2, h / 2, xpos + width, 3)
-            qp.drawLine(xpos + width / 2, h / 2, xpos + width, h - 3)
         elif ttype == 'data2empty':
-            pen.setStyle(Qt.NoPen)
-            qp.setPen(pen)
-
-            points = [QPoint(xpos + width / 2, h / 2),
-                      QPoint(xpos, 3), QPoint(xpos, h - 3)]
+            if hoverLeft:
+                brush.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                brush.setColor(self.SIGNAL_COLOR)
+            points = [QPoint(xpos, 2), QPoint(xpos, h - 2), QPoint(xpos + width / 2, h / 2)]
+            qp.setBrush(brush)
             qp.drawPolygon(QPolygon(points))
 
+            if hoverRight:
+                pen.setColor(self.SIGNAL_COLOR_HOVER)
+            else:
+                pen.setColor(self.SIGNAL_COLOR)
             pen.setStyle(Qt.SolidLine)
             qp.setPen(pen)
             qp.drawLine(xpos + width / 2, h / 2, xpos + width, h / 2)
-            qp.drawLine(xpos, 3, xpos + width / 2, h / 2)
-            qp.drawLine(xpos, h - 3, xpos + width / 2, h / 2)
         else:
             # 'empty2empty' case
             pen.setStyle(Qt.SolidLine)
+            if hoverLeft:
+                pen.setColor(self.SIGNAL_COLOR_HOVER)
+            else: 
+                pen.setColor(self.SIGNAL_COLOR)
             qp.setPen(pen)
-            qp.drawLine(xpos, h / 2, xpos + width, h / 2)
+            qp.drawLine(xpos, h / 2, xpos + width / 2, h / 2)
+
+            if hoverRight:
+                pen.setColor(self.SIGNAL_COLOR_HOVER)
+            else: 
+                pen.setColor(self.SIGNAL_COLOR)
+            qp.setPen(pen)
+            qp.drawLine(xpos + width / 2, h / 2, xpos + width, h / 2)
 
     def _drawState(self, qp, xpos, width, data, hovering=False):
 
@@ -275,16 +312,21 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     sw = SignalTable()
+    sw.setGeometry(300, 300, 800, 400)
     sw.show()
 
-    for i in range(25):
-        signalname = 'Signal-' + str(i)
-        signaldata = []
-        for i in range(120):
-            tokens = [1,2,3,4]
-            rdm.shuffle(tokens)
-            signaldata.append(tokens)
-        sw.addSignal(signalname, signaldata)
+    sw.addSignal('Alpha', [[1],[2],[],[2]])
+    sw.addSignal('Beta', [[1],[1],[],[]])
+    sw.addSignal('Gama', [[],[2],[],[]])
+
+    # for i in range(25):
+    #     signalname = 'Signal-' + str(i)
+    #     signaldata = []
+    #     for i in range(120):
+    #         tokens = [1,2,3,4]
+    #         rdm.shuffle(tokens)
+    #         signaldata.append(tokens)
+    #     sw.addSignal(signalname, signaldata)
 
     app.exec_()
     app.deleteLater()
