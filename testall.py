da852976203e591920901563892c318f75fc4072
Rinse Wester <renzzz@Rinses-MacBook-Pro.local>
2016-10-02 15:15:57 +0200

    Inital class for complete unittest.

---
+++ testall.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Validation of SDFkt using a collection of unittests
+
+author: Rinse Wester
+
+"""
+
+import unittest
+
+import testhsdfgraph
+import testsdfgraph
+
+if __name__ == '__main__':
+    unittest.main()
